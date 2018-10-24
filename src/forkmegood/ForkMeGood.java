946a60fd8ba3f57890cd40952fb64d6e5759a001
djm43@students.ptcollege.edu <>
2018-10-24 15:47:34 -0400

    enjoy hunter

---
+++ src/forkmegood/ForkMeGood.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forkmegood;
+
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+/**
+ * @author dominic mclaughlin
+ */
+public class ForkMeGood {
+
+    public static void main(String[] args) {
+         System.out.print("Steppin on the beach!");
+         Timer timer = new Timer();
+         timer.schedule(new TimerTask(){
+             public void run(){
+                 try{
+                    AudioInputStream audio = AudioSystem.getAudioInputStream(
+                        new File("./Grass_Beach.wav"));
+                    Clip clip = AudioSystem.getClip();
+                    clip.open(audio);
+                    clip.start();
+                } catch(Exception e){
+                    System.out.println("Error Playing Audio File");
+                }
+             }
+         }, 0, 15000);
+    }
+    
+}
